b4a0883f10ad45642426ab90bb3508cd63fa1b98
Jason Felice <jason.m.felice@gmail.com>
2024-11-17 18:35:37 -0500

    Initial build

--- derivation.nix
+++ derivation.nix
@@ -1,13 +1,36 @@
-{ stdenv, lib, ... }:
+{ stdenv, lib, python312Packages, fetchFromGitHub }:
 
-stdenv.mkDerivation rec {
-  pname = "bCNC-nix";
-  version = "0.1.0";
+let
+  pythonPackages = python312Packages;
+in pythonPackages.buildPythonApplication rec {
+  pname = "bCNC";
+  version = "0b3ad63e32d2ef8ffbe1a14345a6821d773f6067";
 
-  src = ./.;
+  src = fetchFromGitHub {
+    owner = "vlachoudis";
+    repo = "bCNC";
+    rev = version;
+    hash = "sha256-OMILyZlqpozW8RXUQnlNLAUubbYjC+30CDBX5pafTzw=";
+  };
+
+  build-system = with pythonPackages; [
+    setuptools
+    wheel
+  ];
+
+  dependencies = with pythonPackages; [
+    tkinter
+    numpy
+    pyserial
+    svgelements
+    #shxparser
+    pillow
+    opencv-python
+    #note about scipy
+  ];
 
   meta = with lib; {
-    description = "TODO: fill me in";
+    description = "GRBL CNC command sender, autoleveler and g-code editor";
     homepage = "https://github.com/eraserhd/bCNC-nix";
     license = licenses.publicDomain;
     platforms = platforms.all;
